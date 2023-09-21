57132c87d6d13d795a92817182972d7c4f811cfd
peleg125 <peleg@mischel.biz>
2023-09-21 13:34:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../proj-misterToy-backend',
+    outDir: '../proj-misterToy-backend/public',
     emptyOutDir: true,
   },
 })
